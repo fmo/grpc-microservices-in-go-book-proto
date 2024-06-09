ea9f30af692b90adf5b1dcbdbb550f45e5678419
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-06-09 13:55:34 +0200

    proto sh

--- protoc.sh
+++ protoc.sh
@@ -19,5 +19,14 @@ protoc --go_out=./golang --go_opt=paths=source_relative \
   ./${SERVICE_NAME}/*.proto
 
 cd golang/${SERVICE_NAME}
-go mod init \
-  github.com/fmo/microservices-proto/golang/${SERVICE_NAME} || true
+go mod init github.com/fmo/microservices-proto/golang/${SERVICE_NAME} || true
+go mod tidy
+cd .../../
+
+git config --global user.email "mustafa.ozyurt@gmail.com"
+git config --global user.name "Mustafa Özyurt"
+
+git add . && git commit -am "proto update" || true
+
+git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
